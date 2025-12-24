ef558a0f7a6a5dfd3a752e44bc5cce48992c8e8f
Everybody <romanov.as@dvfu.ru>
2025-12-24 12:11:45 +1000

    aaaa

--- deploy/scripts/deploy.sh
+++ deploy/scripts/deploy.sh
@@ -55,6 +55,7 @@ cd web_2025
 #python manage.py populate_db
 #python manage.py collectstatic --noinput
 #source $VENV_DIR/bin/activate
+python manage.py makemigrations
 python manage.py dumpdata --indent 2 --output /tmp/data1.json
 python manage.py migrate
 #python manage.py loaddata data.json || echo "data.json не найден — пропускаем"
