c67dd231817227780416e025581f28a0d4786d15
Everybody <romanov.as@dvfu.ru>
2025-12-24 12:27:24 +1000

    Help

--- deploy/scripts/deploy.sh
+++ deploy/scripts/deploy.sh
@@ -55,10 +55,10 @@ cd web_2025
 #python manage.py populate_db
 #python manage.py collectstatic --noinput
 #source $VENV_DIR/bin/activate
-python manage.py makemigrations
-echo "Я не понимаю"
-python manage.py dumpdata --indent 2 --output /tmp/data1.json
 python manage.py migrate
+python manage.py makemigrations
+python manage.py dumpdata --indent 2 --output /tmp/data1.json
+#python manage.py migrate
 #python manage.py loaddata data.json || echo "data.json не найден — пропускаем"
 python manage.py loaddata /tmp/data1.json || echo "data1.json не найден — пропускаем"
 python manage.py collectstatic --noinput
